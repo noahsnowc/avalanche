2baea24292babae8732f51e08af529238792db18
noahsnowc <noahsnowc@users.noreply.github.com>
2022-04-04 16:54:12 -0400

    TODO updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import './App.css';
 import QuillEditor from "./textEditor";
 
+
+/*TODO: add side bar: react side bar: https://medium.com/how-to-react/create-a-sidebar-menu-in-react-js-3463b306ca9a
+TODO: add document saving
+TODO: Add Plot planner
+TODO: add character hub
+TODO: add relationship mapper
+TODO: add auto documenting of characters or places
+*/
+
 function App() {
   return (
   <>
+  
     <QuillEditor />
   </>);
 }
